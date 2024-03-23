ca35d0caabcaebb6f53ed4cdda2698d1ac1d3742
M u h a m a d A g u s <113649505+MhmdAgus21@users.noreply.github.com>
2024-03-23 15:15:39 +0700

    Add files via upload
    Messagers

---
+++ pemprograman-web-lanjut-212310004/src/modules/chapter-1/ChapterOne.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import MessagersUI from '../widgets/messagers/MessegersUI'
+
+export default class ChapterOne extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-center color : white" >
+           Chapter One: The Beginning - Muhamad Agus 
+        </h1>
+        <MessagersUI/>
+      </div>
+    )
+  }
+}
